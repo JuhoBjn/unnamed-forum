8524f24e1de19db834f8a0afb8ee6a0f6fc2121e
Juho Björkman <juho.bjorkman@tuni.fi>
2024-04-15 20:55:49 +0300

    Add user table to the database

---
+++ backend/db/migrations/20240415172352_add-users.sql
@@ -0,0 +1,13 @@
+-- migrate:up
+CREATE TABLE `users` (
+  `id` VARCHAR(36) NOT NULL,
+  `username` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(320) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+-- migrate:down
+DROP TABLE `users`;
